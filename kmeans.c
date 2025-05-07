8d9f0e83a8c0daf004eedcbf8d55b5202ec640fe
ruthwik-coder <74289462+ruthwik-coder@users.noreply.github.com>
2025-05-08 00:19:25 +0530

    errors

--- kmeans.c
+++ kmeans.c
@@ -31,12 +31,7 @@ int resize_margin = 10;
 
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
-    //   TTF_Init();
-      
-    //    if (TTF_Init() < 0) {
-    //     printf("SDL_ttf could not initialize! TTF_Error: %s\n", SDL_GetError());
-    //     return SDL_APP_FAILURE;
-    //    }
+
      
     AppState* app_state = malloc(sizeof(AppState));
     *app_state = (AppState){
@@ -81,109 +76,97 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     return SDL_APP_CONTINUE;
 }
 
+// Add this function to help with precise error tracking
+void log_line_error(const char* function_name, int line_number) {
+    printf("Error at %s (line %d): %s\n", function_name, line_number, SDL_GetError());
+}
 
+// Then in SDL_AppIterate, add specific logging without changing your code
 SDL_AppResult SDL_AppIterate(void* appstate) {
-    
-             // Dynamically allocate memory for the 2D array using malloc
-   int **a = (int **)malloc(1280 * sizeof(int  *));  
-    
-     int **a2 = (int**)malloc(1280 * sizeof(int *));  
-    // Allocate memory for row pointers
-    if ((a == NULL) ||a2==NULL) {
-        printf("Memory allocation failed for rows!\n");
-        return 1;  // Return with an error code if memory allocation fails
-    }
-
-
-    // Allocate memory for each row
-    for (int i = 0; i < 1280; i++) {
-        a[i] = (int *)malloc(720 * sizeof(int));
-          a2[i] = (int *)malloc(720 * sizeof(int));
-        if ((a[i] == NULL) ||a2[i]==NULL) {
-            printf("Memory allocation failed for row %d!\n", i);
-            return 1;  // Return with an error code if memory allocation fails
-        }
-    }
-
-      SDL_PropertiesID props;
     AppState* app_state = (AppState*)appstate;
+    
     SDL_Surface* frame = SDL_AcquireCameraFrame(app_state->camera, NULL);
     if (frame != NULL) {
         if (app_state->texture == NULL) {
-            SDL_SetWindowSize(app_state->window,frame->w,frame->h);
-            app_state->width=frame->w;
-            app_state->height=frame->h;
+            SDL_Log("Creating new texture %dx%d", frame->w, frame->h);
+            SDL_SetWindowSize(app_state->window, frame->w, frame->h);
+            app_state->width = frame->w;
+            app_state->height = frame->h;
             app_state->texture = SDL_CreateTexture(app_state->renderer, frame->format, SDL_TEXTUREACCESS_STREAMING, frame->w, frame->h);
+            if (app_state->texture == NULL) {
+                log_line_error(__func__, __LINE__);
+            }
         } else {
+            // Log point 1: Before updating texture
+            SDL_Log("Point 1: Before updating texture - Error: %s", SDL_GetError());
+            
             SDL_UpdateTexture(app_state->texture, NULL, frame->pixels, frame->pitch);
-      
-
+            
+            // Log point 2: After updating texture
+            SDL_Log("Point 2: After updating texture - Error: %s", SDL_GetError());
         }
 
-
- props = SDL_GetTextureProperties(app_state->texture);
+        // Log point 3: Before getting properties
+        SDL_Log("Point 3: Before getting properties - Error: %s", SDL_GetError());
         
-    if (props == 0) {
-        printf("Failed to get texture properties: %s\n", SDL_GetError());
-       return  SDL_APP_FAILURE;
-    }
-    
-    // Access texture properties
-    int format = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_FORMAT_NUMBER, 0);
-    int access = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_ACCESS_NUMBER, 0);
-    int width = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0);
-    int height = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_HEIGHT_NUMBER, 0);
-    int colorspace = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_COLORSPACE_NUMBER, 0);
-
-SDL_Surface* rgb_frame = SDL_ConvertSurface(frame, SDL_PIXELFORMAT_RGB24);
-if (rgb_frame) {
-    Uint8* pixels = (Uint8*)rgb_frame->pixels;
-    int pitch = rgb_frame->pitch;
-    for (int y = 0; y < rgb_frame->h; y += 20) {
-        for (int x = 0; x < rgb_frame->w; x += 20) {
-            int index = y * pitch + x * 3;
-
-            Uint8 r = pixels[index];
-            Uint8 g = pixels[index + 1];
-            Uint8 b = pixels[index + 2];
-              a[y][x] = r;
-                a2[y][x] = g;
-
-          // printf("Pixel[%d][%d] = R:%d G:%d B:%d\n", y, x, r, g, b);
+        SDL_PropertiesID props = SDL_GetTextureProperties(app_state->texture);
+        
+        // Log point 4: After getting properties
+        SDL_Log("Point 4: After getting properties - Error: %s", SDL_GetError());
+        
+        if (props == 0) {
+            printf("Failed to get texture properties: %s\n", SDL_GetError());
+            return SDL_APP_FAILURE;
         }
-    }
-    SDL_DestroySurface(rgb_frame);  // Free when done
-}
+        
+        // Access texture properties
+        int format = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_FORMAT_NUMBER, 0);
+        int access = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_ACCESS_NUMBER, 0);
+        int width = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0);
+        int height = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_HEIGHT_NUMBER, 0);
+        int colorspace = SDL_GetNumberProperty(props, SDL_PROP_TEXTURE_COLORSPACE_NUMBER, 0);
 
+        SDL_Surface* rgb_frame = SDL_ConvertSurface(frame, SDL_PIXELFORMAT_RGB24);
+        if (rgb_frame) {
+            Uint8* pixels = (Uint8*)rgb_frame->pixels;
+            int pitch = rgb_frame->pitch;
+            for (int y = 0; y < rgb_frame->h; y += 20) {
+                for (int x = 0; x < rgb_frame->w; x += 20) {
+                    int index = y * pitch + x * 3;
+                    Uint8 r = pixels[index];
+                    Uint8 g = pixels[index + 1];
+                    Uint8 b = pixels[index + 2];
+                    // Your commented out code for a[y][x] and a2[y][x]
+                }
+            }
+            SDL_DestroySurface(rgb_frame);
+        }
 
         SDL_ReleaseCameraFrame(app_state->camera, frame);
-    }
-        
-     SDL_SetRenderDrawColorFloat(app_state->renderer, 0.3f, 0.5f, 1.0f, SDL_ALPHA_OPAQUE_FLOAT);
-    SDL_RenderClear(app_state->renderer);
-
-SDL_FRect camera_viewport_f = {
-    .x = (float)camera_viewport.x,
-    .y = (float)camera_viewport.y,
-    .w = (float)camera_viewport.w,
-    .h = (float)camera_viewport.h
-};
-
-      SDL_RenderTexture(app_state->renderer,app_state->texture, NULL, &camera_viewport_f);
-
-// Render all points at once
-//SDL_RenderPoints(app_state->renderer, points, 100);
-    // Free the allocated memory
-    for (int i = 0; i < 1280; i++) {
-        free(a[i]);
-        free (a2[i]) ; // Free each row
+    } else {
+        log_line_error(__func__, __LINE__);
     }
     
-    free(a); 
-    free(a2); // Free the row pointers
- 
+    SDL_SetRenderDrawColorFloat(app_state->renderer, 0.3f, 0.5f, 1.0f, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(app_state->renderer);
+
+    SDL_FRect camera_viewport_f = {
+        .x = (float)camera_viewport.x,
+        .y = (float)camera_viewport.y,
+        .w = (float)camera_viewport.w,
+        .h = (float)camera_viewport.h
+    };
+
+    // Log point 5: Before rendering texture
+    SDL_Log("Point 5: Before rendering texture - Error: %s", SDL_GetError());
+    
+    SDL_RenderTexture(app_state->renderer, app_state->texture, NULL, &camera_viewport_f);
+    
+    // Log point 6: After rendering texture
+    SDL_Log("Point 6: After rendering texture - Error: %s", SDL_GetError());
+
     SDL_RenderPresent(app_state->renderer);
-   //  SDL_DestroySurface(textSurface);
+    
     return SDL_APP_CONTINUE;
 }
 
@@ -228,6 +211,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
         }
     }
 
+
     return SDL_APP_CONTINUE;
 }
 
@@ -243,6 +227,7 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result) {
     if (app_state->texture != NULL) {
         SDL_DestroyTexture(app_state->texture);
     }
+
 
 //TTF_CloseFont(app_state->font);
 
